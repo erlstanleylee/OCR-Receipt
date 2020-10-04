14f4434a6358bdc6abc3733de37eb1e5c3724f4b
Sylvia <sylzheng@ucla.edu>
2020-10-03 22:14:56 -0700

    Initial commit + have Flask server running and can upload pictures to front end React App

---
+++ backend/server.py
@@ -0,0 +1,67 @@
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+import time
+
+import logging
+import pyrebase
+
+#try:
+#    from PIL import Image
+#except ImportError:
+#    import Image
+
+config = { 
+	"apiKey": "AIzaSyBOyikj14f1BWinF96CMO96lS-3ZaNtL-0",
+	"authDomain": "receipt-24ad6.firebaseapp.com",
+	"databaseURL": "https://project-1067210456275.firebaseio.com",
+	"storageBucket": "receipt-24ad6.appspot.com",
+	"serviceAccount":"receipt-24ad6-firebase-adminsdk-qoj51-62596cba24.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+#auth = firebase.auth()
+#user = auth.sign_in_with_email_and_password("", "itiswednesday")
+
+
+def getwords(img):
+	try:
+		from PIL import Image
+	except ImportError:
+		import Image
+	import pytesseract
+	import re
+	#you have to change the path below to the absolute path of the tesseract program
+	
+	pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract' 
+		
+	UnfilteredString = pytesseract.image_to_string(Image.open(img)) #name of the picture you want to analyze
+	
+	print("Unfiltered:")
+	print(UnfilteredString)
+	
+	print("Filtered:")
+	FilteredList = re.findall(".*\.[0-9]{2}\n", UnfilteredString)
+	print(FilteredList)
+	for n, entry in enumerate(FilteredList):
+		FilteredList[n] = re.sub("\$.+\n", "", entry)
+		print(FilteredList[n])
+
+
+@app.route('/', methods=['GET','POST'])
+def fp():
+    if request.method == 'POST':
+        img = request.files['file']
+        getwords(img)
+        return redirect(url_for('hello'))
+		
+@app.route('/hello')
+def hello():
+	return('hello world')
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ receipt/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>
+          Upload a photo of your receipt!
+        </h3>
+        <form method="post" encType="multipart/form-data">
+			<input type="file" name="file" id="file"></input>
+			<input type="submit" value="Upload"></input>
+		</form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
